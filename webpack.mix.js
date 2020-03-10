533738d55da679986257eff8e74163e9983b142a
708-U <708.u.biz@gmail.com>
2020-03-10 14:29:52 +0000

    Add browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,15 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        proxy: {
+            target: "http://nginx"
+        },
+        files: [
+            "./resources/**/*.blade.php",
+            "./public/**/*"
+        ],
+        open: false,
+        reloadOnRestart: true
+    });
