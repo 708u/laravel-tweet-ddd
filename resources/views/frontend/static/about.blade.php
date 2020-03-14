a795d5bc877722f2e1c0fccd117ed91ddc0ac85f
708-U <708.u.biz@gmail.com>
2020-03-14 16:30:11 +0000

    Add about page

---
+++ resources/views/frontend/static/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'about')
+
+@section('content')
+<h1 class="text-center">About</h1>
+<p class="text-center">
+    Laravelでtwitter風アプリケーションをドメイン駆動設計で開発するプロジェクト
+</p>
+@endsection
