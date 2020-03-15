121ef724cf628f759c3e3f8d53f8eb0e7e495814
708-U <708.u.biz@gmail.com>
2020-03-15 08:48:11 +0000

    Add showLoginFrom route

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name('frontend.')->namespace('frontend')->group(function () {
+Route::name('frontend.')->namespace('Frontend')->group(function () {
     Route::name('static.')->group(function () {
         Route::view('/', 'frontend.static.home')->name('home');
         Route::view('/about', 'frontend.static.about')->name('about');
         Route::view('/help', 'frontend.static.help')->name('help');
     });
+
+    Route::name('auth.')->namespace('Auth')->group(function() {
+        Route::get('/login', 'LoginController@showLoginForm')->name('login');
+    });
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
