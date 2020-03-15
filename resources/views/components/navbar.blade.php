37236ff9f21f3bb90d9a0babd96476f75a059472
708-U <708.u.biz@gmail.com>
2020-03-15 04:30:35 +0000

    Add about and help page links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -18,6 +18,12 @@
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('frontend.static.about') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('frontend.static.help') }}">Help</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                     @if (Route::has('register'))
