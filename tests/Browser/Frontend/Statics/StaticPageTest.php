8067eb9b867f3cff49cc51bcae4ddf03834217a4
708-U <708.u.biz@gmail.com>
2020-03-15 07:55:45 +0000

    Change static test to visit per pages via home

--- tests/Browser/Frontend/Statics/StaticPageTest.php
+++ tests/Browser/Frontend/Statics/StaticPageTest.php
@@ -34,7 +34,8 @@ class StaticPageTest extends DuskTestCase
     public function testVisitAboutPage()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/about')
+            $browser->visit('/')
+                ->clickLink('About')
                 ->assertRouteIs('frontend.static.about')
                 ->assertTitle($this->getTitle('About'));
         });
@@ -48,7 +49,8 @@ class StaticPageTest extends DuskTestCase
     public function testVisitHelpPage()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/help')
+            $browser->visit('/')
+                ->clickLink('Help')
                 ->assertRouteIs('frontend.static.help')
                 ->assertTitle($this->getTitle('Help'));
         });
