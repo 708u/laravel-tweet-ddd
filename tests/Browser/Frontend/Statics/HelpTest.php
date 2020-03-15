425becb480f53c429cd1abab452444defb3c5d87
708-U <708.u.biz@gmail.com>
2020-03-15 01:18:29 +0000

    replace word of page from static page tests

--- tests/Browser/Frontend/Statics/HelpTest.php
+++ tests/Browser/Frontend/Statics/HelpTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class HelpPageTest extends DuskTestCase
+class HelpTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
