ae482dfa1ff63e15e5191d899d4ae6d2a23fd8c6
708-U <708.u.biz@gmail.com>
2020-03-18 02:03:36 +0000

    Bind Abstract and concrete of UuidGen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use Domain\Application\Contract\Uuid\UuidGeneratable;
 use Illuminate\Support\ServiceProvider;
+use Infrastructure\Application\Uuid\UuidGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $bindings = [
+        UuidGeneratable::class => UuidGenerator::class
+    ];
+
     /**
      * Register any application services.
      *
