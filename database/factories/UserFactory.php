560f2323429ca442493d474c7120049971ea3965
708-U <708.u.biz@gmail.com>
2020-03-19 00:54:18 +0000

    Add constructed uuid to factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Eloquent\User;
+use Domain\Application\Contract\Uuid\UuidGeneratable;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -18,6 +19,7 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'uuid'              => resolve(UuidGeneratable::class)->generate(),
         'name'              => $faker->name,
         'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
