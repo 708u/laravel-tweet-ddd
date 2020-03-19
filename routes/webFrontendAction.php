de79a388dd2654c9d0f2b818ee42acfc4eef8f53
708-U <708.u.biz@gmail.com>
2020-03-19 00:58:18 +0000

    Change user show route parameter to string

--- routes/webFrontendAction.php
+++ routes/webFrontendAction.php
@@ -31,5 +31,5 @@ Route::name('auth.')->namespace('Auth')->group(function () {
 
 Route::name('user.')->namespace('User')->middleware('auth')->group(function () {
     Route::get('/home', 'ShowHomeAction')->name('home');
-    Route::get('/users/{id}', 'ShowUserAction')->name('show');
+    Route::get('/users/{uuid}', 'ShowUserAction')->name('show');
 });
