51363b72392ee488a02306a92acfcc2e394bd49b
NoayaUda <uda@arsaga.jp>
2020-03-19 07:45:58 +0000

    Add unit test of Entity

---
+++ tests/Unit/domain/Model/Entity/Base/EntityTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Unit\domain\Model\Entity\Base;
+
+use Tests\TestCase;
+use Domain\Model\Entity\Base\Entity;
+use Domain\Model\ValueObject\Base\Identifier;
+
+class EntityTest extends TestCase
+{
+    private string $plainIdentifier = '901e66be-91ec-499e-b91a-70260f3c7ba6';
+
+    private Identifier $identifier;
+
+    private Entity $entity;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Create anonymous entity for testing
+        $this->identifier = $this->createAnonymousIdentifier($this->plainIdentifier);
+        $this->entity = new class($this->identifier) extends Entity {
+            public function __construct(Identifier $identifier)
+            {
+                $this->identifier = $identifier;
+            }
+        };
+    }
+
+    /**
+     * @group entity
+     * @return void
+     */
+    public function testCanDetermineEquals()
+    {
+        $this->assertTrue($this->entity->equals($this->identifier));
+        // Determine if when Created Identifier has another instance-id
+        $this->assertTrue($this->entity->equals($this->createAnonymousIdentifier($this->plainIdentifier)));
+
+        $anotherIdentifier = $this->createAnonymousIdentifier('901e6a68-fbea-415b-af75-76bbed08fdb1');
+        $this->assertFalse($this->entity->equals($anotherIdentifier));
+    }
+
+    /**
+     * Create fake Identifier for testing.
+     *
+     * @param string $uuid
+     * @return Identifier
+     */
+    private function createAnonymousIdentifier(string $uuid): Identifier
+    {
+        return new class ($uuid) extends Identifier {};
+    }
+}
