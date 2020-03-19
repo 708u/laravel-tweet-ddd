7a715121ed078e76b72e2d5d57f09d6901cdd548
NoayaUda <uda@arsaga.jp>
2020-03-19 08:12:56 +0000

    Add Indetifier test

---
+++ tests/Unit/domain/Model/ValueObject/Base/IdentifierTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Unit\domain\Model\ValueObject\Base;
+
+use Tests\TestCase;
+use Domain\Model\ValueObject\Base\Identifier;
+use DomainException;
+
+class IdentifierTest extends TestCase
+{
+    private string $plainIdentifier = '901e66be-91ec-499e-b91a-70260f3c7ba6';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @grout value-object
+     * @return void
+     */
+    public function testCanDetermineEquals()
+    {
+        $identifier = $this->createAnonymousIdentifier($this->plainIdentifier);
+        $identifier2 = $this->createAnonymousIdentifier($this->plainIdentifier);
+
+        $this->assertTrue($identifier->equals($identifier2));
+    }
+
+    /**
+    * @test
+    * @group value-object
+    *
+    * @return void
+    */
+    public function testShouldThrowExceptionIfInvalidArgumentGigen()
+    {
+        $this->expectException(DomainException::class);
+
+        // Should throw exception because of foobar.
+        $this->createAnonymousIdentifier('foobar');
+    }
+
+    /**
+     * Create fake Identifier for testing.
+     *
+     * @param string $uuid
+     * @return Identifier
+     */
+    private function createAnonymousIdentifier(string $uuid): Identifier
+    {
+        return new class($uuid) extends Identifier {
+        };
+    }
+}
