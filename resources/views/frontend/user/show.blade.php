6a74726cf9ba80eb7541044f3caf1a6ab7205515
708-U <708.u.biz@gmail.com>
2020-03-21 09:46:45 +0000

    Add aside to show page

--- resources/views/frontend/user/show.blade.php
+++ resources/views/frontend/user/show.blade.php
@@ -5,10 +5,13 @@
 @endsection
 
 @section('content')
-<div>
-    {{ $user->userName }}
-</div>
-<div>
-    {{ $user->email }}
+<div class="container mt-3">
+    <div class="row">
+        <aside class="col-md-4">
+            <section class="user_info">
+                <h1>{{ $user->userName }} {{ $user->email }}</h1>
+            </section>
+        </aside>
+    </div>
 </div>
 @endsection
