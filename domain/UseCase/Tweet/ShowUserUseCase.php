bad894ef32482a0e52a6f8e1b6acba84a2d79d4e
708-U <708.u.biz@gmail.com>
2020-03-21 05:45:47 +0000

    Add showUserUseCase

---
+++ domain/UseCase/Tweet/ShowUserUseCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Domain\UseCase\Tweet;
+
+use Domain\Model\DTO\Tweet\UserDTO;
+use Domain\Model\ValueObject\Tweet\Identifier\UserId;
+use Domain\Repository\Contract\Tweet\UserRepository;
+
+class ShowUserUseCase
+{
+    private UserRepository $user;
+
+    public function __construct(UserRepository $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Find User to show its details.
+     *
+     * @param string $identifier
+     * @return UserDTO
+     */
+    public function execute(string $identifier): UserDTO
+    {
+        return $this->user->find(new UserId($identifier))->toDto();
+    }
+}
