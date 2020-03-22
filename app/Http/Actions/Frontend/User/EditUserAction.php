e6775a8368f6dd39112f38e86298a5b5e91802e0
708-U <708.u.biz@gmail.com>
2020-03-22 12:52:47 +0000

    Fix edit action name

--- app/Http/Actions/Frontend/User/EditUserAction.php
+++ app/Http/Actions/Frontend/User/EditUserAction.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Responders\Frontend\User\EditUserResponder;
 use Domain\UseCase\Tweet\ShowUserUseCase;
 
-class ShowEditingUserAction extends Controller
+class EditUserAction extends Controller
 {
     private ShowUserUseCase $useCase;
 
