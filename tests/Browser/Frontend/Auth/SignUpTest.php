8fc36d814046014a2736b42d03ba2cb606d29a21
708-U <708.u.biz@gmail.com>
2020-03-22 03:21:40 +0000

    Add flash message test of signup

--- tests/Browser/Frontend/Auth/SignUpTest.php
+++ tests/Browser/Frontend/Auth/SignUpTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Frontend\Auth;
 
+use App\Eloquent\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -25,6 +26,7 @@ class SignUpTest extends DuskTestCase
         $email = 'foobar@example.com';
         $password = '123456789';
         $this->browse(function (Browser $browser) use ($name, $email, $password) {
+            $flashMessage = 'Welcome! Your Account Successfully Created!';
             $browser->visit('/signup')
                 ->assertRouteIs('frontend.auth.signup')
                 ->type('name', $name)
@@ -32,12 +34,14 @@ class SignUpTest extends DuskTestCase
                 ->type('password', $password)
                 ->type('password_confirmation', $password)
                 ->press('Register')
-                ->assertRouteIs('frontend.user.home')
+                ->assertRouteIs('frontend.user.show', $uuid = User::first()->uuid) // FIXME: should remove dependencies
+                ->assertSee($flashMessage) // welcome flash message
                 ->assertAuthenticated()
                 ->visit('/signup')
-                ->assertRouteIs('frontend.user.home') // redirect home if already signed up
+                ->assertRouteIs('frontend.user.show', $uuid) // redirect user page if already signed up
+                ->assertDontSee($flashMessage) // flash message disappeared
                 ->visit('/login')
-                ->assertRouteIs('frontend.user.home'); // also redirect home if already signed up
+                ->assertRouteIs('frontend.user.show', $uuid); // also redirect user page if already signed up
         });
 
         $this->assertDatabaseHas(
