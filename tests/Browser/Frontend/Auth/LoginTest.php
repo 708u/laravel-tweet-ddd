7f3fcacdf1017608692cfa2027c786b729f1cd19
708-U <708.u.biz@gmail.com>
2020-03-22 07:47:43 +0000

    Move AssertSeeLink test to loginTest

--- tests/Browser/Frontend/Auth/LoginTest.php
+++ tests/Browser/Frontend/Auth/LoginTest.php
@@ -27,6 +27,10 @@ class LoginPageTest extends DuskTestCase
         $user = factory(User::class)->create(['password' => app(Hashable::class)->make($plainPassword)]);
         $this->browse(function (Browser $browser) use ($user, $plainPassword) {
             $browser->visit('/login')
+                ->assertSeeLink('Home') // Can see dropdown menu before log in.
+                ->assertSeeLink('Help')
+                ->assertSeeLink('Login')
+                ->assertSeeLink('About')
                 ->assertSee('Login')
                 ->assertRouteIs('frontend.auth.login')
                 ->type('email', $user->email)
@@ -35,7 +39,11 @@ class LoginPageTest extends DuskTestCase
                 ->assertRouteIs('frontend.user.show', $user->uuid)
                 ->assertSee($flashMessage = 'You are logged in!') // flash message
                 ->refresh()
-                ->assertDontSee($flashMessage);
+                ->assertDontSee($flashMessage)
+                ->click('#navbarDropdown')
+                ->assertSeeLink('Profile') // Can see dropdown menu after logged in.
+                ->assertSeeLink('Setting')
+                ->assertSeeLink('Logout');
         });
     }
 }
