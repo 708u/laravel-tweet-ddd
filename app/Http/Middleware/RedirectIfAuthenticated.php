1258528627d17c16e6368c146f4d6678d23ace7a
708-U <708.u.biz@gmail.com>
2020-03-22 03:20:03 +0000

    Change aiming to redirect route

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            $uuid = Auth::id();
+            return redirect(RouteServiceProvider::HOME . "/$uuid");
         }
 
         return $next($request);
