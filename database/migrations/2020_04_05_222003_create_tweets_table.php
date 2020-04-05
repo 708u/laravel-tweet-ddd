04c00e28bc1b90af34360a2735a44e56c2975953
708-U <708.u.biz@gmail.com>
2020-04-05 13:25:47 +0000

    Add tweets model and migration

---
+++ database/migrations/2020_04_05_222003_create_tweets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->string('content', 140);
+            $table->uuid('user_uuid');
+            $table->foreign('user_uuid')->references('uuid')->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
