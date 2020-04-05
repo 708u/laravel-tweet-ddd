bd8a27eb2fbe56b9c6140f76cecd9c5871a83519
708-U <708.u.biz@gmail.com>
2020-04-05 13:43:02 +0000

    Add TweetsTableSeeder

---
+++ database/seeds/TweetsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Eloquent\User;
+use Illuminate\Database\Seeder;
+
+class TweetsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $users->each(function ($users) {
+            $users->tweets()->createMany([
+                ['content' => 'hello world!'],
+                ['content' => 'こんにちは、世界!'],
+            ]);
+        });
+    }
+}
