cce8ebf3d72202d9759bc9f24a846437ea5bc61d
708-U <708.u.biz@gmail.com>
2020-04-06 00:23:51 +0000

    Add fillable to tweet eloquent

--- app/Eloquent/Tweet.php
+++ app/Eloquent/Tweet.php
@@ -5,6 +5,15 @@ namespace App\Eloquent;
 class Tweet extends UuidModel
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content', 'user_uuid',
+    ];
+
+    /**
      * User Relationship.
      *
      * @return void
