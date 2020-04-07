f08acc9a931e3bb463d407c2164e2e776d1f3f59
708-U <708.u.biz@gmail.com>
2020-04-07 01:37:29 +0000

    Add uuid gen to TweetFactory

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,10 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Eloquent\Tweet;
+use Domain\Application\Contract\Uuid\UuidGeneratable;
 use Faker\Generator as Faker;
 
 $factory->define(Tweet::class, function (Faker $faker) {
     return [
+        'uuid'    => resolve(UuidGeneratable::class)->nextIdentifier(),
         'content' => $faker->sentence,
     ];
 });
