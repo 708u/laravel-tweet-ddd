701991e4e409b2577a05f0871d63f07c7ffcd5c7
708-U <708.u.biz@gmail.com>
2020-04-08 02:59:36 +0000

    Add InMemoryRepositoryTest

---
+++ tests/Unit/domain/Repository/Base/InMemoryRepositoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\domain\Repository\Base;
+
+use Domain\Model\Entity\Base\Entity;
+use Domain\Model\ValueObject\Base\Identifier;
+use Tests\Helper\Domain\Model\Entity\Base\AbstractEntityMock;
+use Tests\Helper\Domain\Model\Repository\Base\AbstractInMemoryRepositoryMock;
+use Tests\Helper\Domain\Model\ValueObject\Base\AbstractIdentifierMock;
+use Tests\Helper\Utils\CanReferUnaccessible;
+use Tests\TestCase;
+
+/**
+ * @internal
+ */
+class InMemoryRepositoryTest extends TestCase
+{
+    use CanReferUnaccessible;
+
+    private AbstractInMemoryRepositoryMock $repository;
+
+    private string $plainIdentifier = '901e66be-91ec-499e-b91a-70260f3c7ba6';
+
+    private Identifier $identifier;
+
+    private Entity $entity;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = resolve(AbstractInMemoryRepositoryMock::class);
+        $this->identifier = new AbstractIdentifierMock($this->plainIdentifier);
+        $this->entity = new AbstractEntityMock($this->identifier);
+    }
+
+    /**
+     * @group repository
+     *
+     * @return void
+     */
+    public function testSave()
+    {
+        $this->repository->save($this->entity);
+        $savedEntity = $this->getUnaccessibleProperty($this->repository, 'repository')[AbstractInMemoryRepositoryMock::class][$this->plainIdentifier];
+        $this->assertSame($this->entity, $savedEntity);
+    }
+}
