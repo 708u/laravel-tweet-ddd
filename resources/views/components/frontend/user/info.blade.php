3c54753686288fab9df69adc9e317fb8086a67ba
708-U <708.u.biz@gmail.com>
2020-04-11 08:24:01 +0000

    Remove comment and change count to total

--- resources/views/components/frontend/user/info.blade.php
+++ resources/views/components/frontend/user/info.blade.php
@@ -1,6 +1,5 @@
 <section class="user_info">
-    {{-- TODO: Hardcoded. MUST fix --}}
     <h1>{{ $user->userName }}</h1>
     <span><a href="{{ route('frontend.user.show', ['uuid' => $user->identifier]) }}">view my profile</a></span>
-    <span>{{ $feeds->total() }} {{ Str::plural('tweet', $feeds->count()) }}.</span>
+    <span>{{ $feeds->total() }} {{ Str::plural('tweet', $feeds->total()) }}.</span>
 </section>
