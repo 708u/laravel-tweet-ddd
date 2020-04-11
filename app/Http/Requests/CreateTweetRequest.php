7e0a87e9765cbd4ce7fc3245671919561bcc2582
708-U <708.u.biz@gmail.com>
2020-04-11 10:04:11 +0000

    apply formatter

--- app/Http/Requests/CreateTweetRequest.php
+++ app/Http/Requests/CreateTweetRequest.php
@@ -24,7 +24,7 @@ class CreateTweetRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => 'required|max:140'
+            'content' => 'required|max:140',
         ];
     }
 
