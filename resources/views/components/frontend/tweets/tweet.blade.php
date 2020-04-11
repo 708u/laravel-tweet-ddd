08f8dd0d3250d1b8f0fd9e22a905ec6fd9ba7df9
708-U <708.u.biz@gmail.com>
2020-04-11 12:49:14 +0000

    add delete form

--- resources/views/components/frontend/tweets/tweet.blade.php
+++ resources/views/components/frontend/tweets/tweet.blade.php
@@ -1,5 +1,11 @@
 <li id="tweet-{{ $tweet->identifier }}">
     <span class="user"><a href="{{ route('frontend.user.show', ['uuid' => $tweet->userId]) }}">{{ $tweet->username }}</a></span>
     <span class="content">{{ $tweet->tweetContent }}</span>
-    <span class="timestamp">Posted {{ $tweet->timestamp }}.</span>
+    <form method="POST" action="{{ route('frontend.tweet.destroy', ['tweetUuid' => $tweet->identifier]) }} }}">
+        @csrf
+        @method('DELETE')
+        <span class="timestamp">Posted {{ $tweet->timestamp }}.
+            <button type="submit" class="btn">Delete</button>
+        </span>
+    </form>
 </li>
