68b8cb38745c1daa3a57ed5dad7443a696bccc74
708-U <708.u.biz@gmail.com>
2020-04-13 02:33:45 +0000

    Add filesystem config for minio

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,16 +56,15 @@ return [
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key'    => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url'    => env('AWS_URL'),
+            'driver'                  => 's3',
+            'endpoint'                => env('AWS_URL'),
+            'use_path_style_endpoint' => env('AWS_PATH_STYLE_ENDPOINT', false),
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION'),
+            'bucket'                  => env('AWS_BUCKET'),
         ],
-
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Symbolic Links
