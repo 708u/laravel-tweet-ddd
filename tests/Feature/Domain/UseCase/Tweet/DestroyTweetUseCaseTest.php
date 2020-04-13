ca1d0840296ff6d227e8e9b83641f0d4c14dabd6
708-U <708.u.biz@gmail.com>
2020-04-13 00:24:36 +0000

    Add test of testFailToDestroyIfOtherUserTryToDo

--- tests/Feature/Domain/UseCase/Tweet/DestroyTweetUseCaseTest.php
+++ tests/Feature/Domain/UseCase/Tweet/DestroyTweetUseCaseTest.php
@@ -7,6 +7,7 @@ use Domain\Model\ValueObject\Tweet\TweetContent\TweetContent;
 use Domain\Repository\Contract\Tweet\TweetRepository;
 use Domain\Repository\Contract\Tweet\UserRepository;
 use Domain\UseCase\Tweet\DestroyTweetUseCase;
+use DomainException;
 use Infrastructure\DomainModelGeneratable\Eloquent\Tweet\TweetGeneratable;
 use Infrastructure\DomainModelGeneratable\Eloquent\Tweet\UserGeneratable;
 use Infrastructure\Repository\Tweet\InMemory\InMemoryTweetRepository;
@@ -54,5 +55,31 @@ class DestroyTweetUseCaseTest extends TestCase
 
         // assert tweet was destroyed.
         $this->assertTrue($this->tweet->hasNotSaved($tweet));
+    }
+
+    /**
+     * @group usecase
+     *
+     * @return void
+     */
+    public function testFailToDestroyIfOtherUserTryToDo()
+    {
+        $user = $this->generateUserFromEloquent(factory(User::class)->make());
+        $this->user->create($user);
+
+        $tweet = $user->tweet(TweetContent::factory('hello world!'));
+        $this->tweet->create($tweet);
+
+        // assert tweet was created.
+        $this->assertTrue($this->tweet->hasSaved($tweet));
+
+        $otherUser = $this->generateUserFromEloquent(factory(User::class)->make());
+        $this->user->create($otherUser);
+
+        // Other user try to destroy tweet
+        $this->expectException(DomainException::class);
+        $this->expectErrorMessage('You try to delete a tweet owned other users.');
+
+        $this->useCase->execute($tweet->identifierAsString(), $otherUser->identifierAsString());
     }
 }
