041a598be5dbf8374b84884f28dcbb5b035bac5e
708-U <708.u.biz@gmail.com>
2020-04-14 00:45:23 +0000

    Create PostedPicture model

---
+++ database/migrations/2020_04_14_094058_create_posted_pictures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostedPicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posted_pictures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posted_pictures');
+    }
+}
