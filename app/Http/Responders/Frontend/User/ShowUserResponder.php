52e3d1a11484be17c0dcd4664d72f71a384ed9fc
708-U <708.u.biz@gmail.com>
2020-04-18 15:46:42 +0000

    Fix show user responder

--- app/Http/Responders/Frontend/User/ShowUserResponder.php
+++ app/Http/Responders/Frontend/User/ShowUserResponder.php
@@ -4,6 +4,8 @@ namespace App\Http\Responders\Frontend\User;
 
 use App\Http\Responders\Base\Responder;
 use Domain\Model\DTO\Tweet\UserDTO;
+use Domain\Model\ValueObject\Tweet\Identifier\UserId;
+use Domain\Query\Contract\Tweet\PostedTweetQuery;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\Response;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -11,12 +13,18 @@ use Illuminate\Support\Collection;
 
 class ShowUserResponder extends Responder implements Responsable
 {
-    private UserDTO $user;
+    private PostedTweetQuery $query;
 
-    private Collection $tweets;
+    private UserDTO $user;
 
     /** @var int amount of items shown in per pages */
     private int $perPage = 30;
+
+    public function __construct(PostedTweetQuery $query)
+    {
+        parent::__construct();
+        $this->query = $query;
+    }
 
     /**
      * Create response
@@ -28,9 +36,11 @@ class ShowUserResponder extends Responder implements Responsable
     {
         $page = (int) $request->query('page');
 
+        $tweets = collect($this->query->postedTweets(new UserId($this->user->identifier)));
+
         $paginatedTweets = app()->makeWith(LengthAwarePaginator::class, [
-            'items'   => $this->tweets->forPage($page, $this->perPage),
-            'total'   => $this->tweets->count(),
+            'items'   => $tweets->forPage($page, $this->perPage),
+            'total'   => $tweets->count(),
             'perPage' => $this->perPage,
         ]);
 
