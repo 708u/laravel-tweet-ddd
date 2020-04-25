a66aeb45f70e44cffb95dab6239ca0d93cdb1137
708-U <708.u.biz@gmail.com>
2020-04-25 05:06:24 +0000

    Apply uikit selecter to singup and edit profile

--- resources/views/components/frontend/user/profile.blade.php
+++ resources/views/components/frontend/user/profile.blade.php
@@ -1,16 +1,17 @@
-<div class="col-md-8">
-    <div class="card">
-        <div class="card-header">{{ $profileCardName }}</div>
+<div class="uk-margin-large-top uk-margin-large-bottom" uk-grid>
+    <div class="uk-card uk-card-default uk-width-xlarge@s uk-margin-auto">
+        <div class="uk-card-header">
+            <h3 class="uk-card-title uk-text-lead">{{ $profileCardName }}</h3>
+        </div>
 
-        <div class="card-body">
-            <form method="POST" action="{{ $formAction }}">
+        <div class="uk-card-body">
+            <form method="POST" action="{{ $formAction }}" class="uk-form-horizontal">
                 {{ $httpMethod }}
                 @csrf
-                <div class="form-group row">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') ?? $userName }}" required autocomplete="name" autofocus>
+                <div class="uk-margin">
+                    <label for="name" class="uk-form-label">{{ __('Name') }}</label>
+                    <div class="uk-form-controls">
+                        <input id="name" type="name" class="uk-input @error('name') is-invalid @enderror" name="name" value="{{ old('name') ?? $userName }}" required autocomplete="name" autofocus>
 
                         @error('name')
                             <span class="invalid-feedback" role="alert">
@@ -20,11 +21,10 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $email }}" required autocomplete="email">
+                <div class="uk-margin">
+                    <label for="email" class="uk-form-label">{{ __('E-Mail Address') }}</label>
+                    <div class="uk-form-controls">
+                        <input id="email" type="email" class="uk-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $email }}" required autocomplete="email">
 
                         @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -34,11 +34,10 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                <div class="uk-margin">
+                    <label for="password" class="uk-form-label">{{ __('Password') }}</label>
+                    <div class="uk-form-controls">
+                        <input id="password" type="password" class="uk-input @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                         @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -48,20 +47,17 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                <div class="uk-margin">
+                    <label for="password-confirm" class="uk-form-label">{{ __('Confirm Password') }}</label>
+                    <div class="uk-form-controls">
+                        <input id="password-connfirm" type="password" class="uk-input" name="password_confirmation" required autocomplete="new-password">
                     </div>
                 </div>
 
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ $actionButton }}
-                        </button>
-                    </div>
+                <div class="uk-form-controls">
+                    <button type="submit" class="uk-form-width-medium uk-button uk-button-primary uk-width-1-1">
+                        {{ $actionButton }}
+                    </button>
                 </div>
             </form>
         </div>
