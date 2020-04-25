c25e65610105fe06d3f40b373cc8d5250ae90339
708-U <708.u.biz@gmail.com>
2020-04-25 05:41:36 +0000

    Fix profile box display as flexbox

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center py-5">
-        <x-frontend.user.profile :profileCardName="$profileCardTitle" :actionButton="$actionButton" userName="" email="">
-            <x-slot name="formAction">
-                {{ route('frontend.auth.signup') }}
-            </x-slot>
-            <x-slot name="httpMethod"></x-slot>
-        </x-frontend.user.profile>
-    </div>
+<div class="uk-margin-large-top uk-margin-large-bottom uk-flex uk-flex-center">
+    <x-frontend.user.profile :profileCardName="$profileCardTitle" :actionButton="$actionButton" userName="" email="">
+        <x-slot name="formAction">
+            {{ route('frontend.auth.signup') }}
+        </x-slot>
+        <x-slot name="httpMethod"></x-slot>
+    </x-frontend.user.profile>
 </div>
 @endsection
