4077d043f9154511e1b5ed1d21d2fc8448d652b0
708-U <708.u.biz@gmail.com>
2020-04-25 03:42:53 +0000

    include uikit js to app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
     .copyDirectory("resources/img/**/*", "public/img")
     .version()
     .browserSync({
