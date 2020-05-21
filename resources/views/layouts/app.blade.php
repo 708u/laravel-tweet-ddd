d5ecae85e405fe28e011dac5a734f70f5c68b56f
708-U <708.u.biz@gmail.com>
2020-05-21 08:39:29 +0000

    surround mix() with asset()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset(mix('/css/app.css')) }}">
     <x-partial.shim/>
     @yield('head')
     @yield('title')
@@ -16,6 +16,6 @@
     @yield('foot')
     <x-partial.footer/>
 
-    <script src="{{ asset('/js/app.js') }}"></script>
+    <script src="{{ asset(mix('/js/app.js')) }}"></script>
 </body>
 </html>
