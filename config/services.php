453ff167037ce90b29bdc06067a8cc86e9178f9c
708-U <708.u.biz@gmail.com>
2020-05-24 13:17:39 +0000

    add aws-sdk and guzzle http for aws ses

--- config/services.php
+++ config/services.php
@@ -25,9 +25,9 @@ return [
     ],
 
     'ses' => [
-        'key'    => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'key'    => env('AWS_SES_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SES_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_SES_DEFAULT_REGION'),
     ],
 
 ];
